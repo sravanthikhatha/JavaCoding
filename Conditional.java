781bce14fa17345c52cf80eb5ade888d3b7bf88e
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:02:18 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,22 @@
+package com.javacoding;
+
+public class Conditional {
+
+	public static void main(String[]args) {
+		int i = 20 ;
+		int j = 6;
+		System.out.println(i < j && i==j);
+		
+		/*
+		 * &&//multiplication
+		 * true- true = true
+		 * true - false = false 
+		 * false -  false = false
+		 * false - false = false 
+		 * 
+		 * 
+		 * 
+		 */
+
+}
+}
