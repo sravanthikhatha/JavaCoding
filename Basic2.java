7f595caeca3d95dc585568543829eabe19121096
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:01:13 +0530

    Add files via upload

---
+++ Basic2.java
@@ -0,0 +1,16 @@
+package com.javacoding;
+
+public class Basic2 {
+
+	public static void main(String[] args) {
+	// variables = names , Datatypes = int ,strings{ "sravanthi"}=data 
+		// int sravanthi = 1 sravanthi ane oka variable lo int ni store chestunavu
+	//	while printing variable we dont use double couts[""]
+		String sravs = "teju";
+		int num = 1;
+		
+		System.out.println(sravs);
+		System.out.println(num)	;
+}
+
+}
