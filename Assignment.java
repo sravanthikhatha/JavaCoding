62f69d97fa89169d734cf4de4aa0ae397efa7ed9
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:08:21 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,18 @@
+package com.javacoding;
+
+public class Assignment {
+	public static void main(String[]args) {
+	int  i = 20 ;
+	int j = 6 ;
+	// i = i +5 ;
+	// i += 5 ;
+	// i -= 5;
+	// i *= 5;
+	// i /=5;
+	   i /=5;
+	
+	
+	System.out.println(i);
+	}
+
+}
