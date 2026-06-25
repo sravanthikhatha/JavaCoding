13cb2f954e1eb458033f64b73aa30af9b2336267
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:05:48 +0530

    Add files via upload

---
+++ Palindrome1.java
@@ -0,0 +1,33 @@
+package com.javacoding;
+
+public class Palindrome1 {
+
+	public static void main(String[] args) {
+//	String s ="aba";
+//	String ans = "";
+//	for(int i = s.length()-1;i>=0;i--) {
+//		ans = ans+ s.charAt(i);
+//	}
+//		if(ans.equals(s)) {
+//			System.out.println("yes , this is a palindrome");
+//		}
+//		else {
+//			System.out.println("no , this is not a palindrome");
+//		}
+	
+
+	
+
+//for array length arr.length
+//for string length s.length()
+
+// other way 
+		for(int i = 0;i<5;i++) {
+			System.out.println(i);
+		//	if(i==2) {
+				break;
+		//	}
+		}
+		
+}
+	}
