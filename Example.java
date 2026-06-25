6c977080da887eb6fb1088df4af606c7b15c8e99
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:04:46 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,11 @@
+package com.javacoding;
+
+public class Example {
+	public static void main(String[]args) {
+		int pincode ;//defined
+		pincode = 500584; // initializing
+		pincode = 500595; // reasigning
+		System.out.println(pincode);
+	}
+}
+
