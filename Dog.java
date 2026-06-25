03ab620792b98b2b0aef1798d6e9ffc4437253e7
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:03:52 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,16 @@
+package com.javacoding;
+
+public class Dog  {
+	//varaibles
+			public String breed = "german sherphad";
+			public int height = 35 ;
+			public int weight = 40 ;
+			//method
+			public static void main(String[]args) {
+			System.out.println ("Hello World");
+			//object
+			Dog dog = new Dog();
+			System.out.println(dog.breed);
+			}
+
+}
