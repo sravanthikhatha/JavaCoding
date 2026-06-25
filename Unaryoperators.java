91c9719dc60189e7c681eb4ff07e099cbea9ebf1
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:08:01 +0530

    Add files via upload

---
+++ Unaryoperators.java
@@ -0,0 +1,19 @@
+package com.javacoding;
+
+public class Unaryoperators {
+	public static void main(String[]args) {
+		int i = 20;
+		int j = 6 ;
+		
+		System.out.println(i); //20
+		System.out.println(++i); //21
+        System.out.println(i++); //21
+        System.out.println(i); //22
+        System.out.println(j); //6
+        System.out.println(--j); //5
+        System.out.println(j--); //5
+        System.out.println(j); //4   
+
+	}
+
+}
