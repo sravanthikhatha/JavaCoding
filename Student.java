699ae28a8b35e549542eddbabdfead686cd7d672
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:06:27 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package com.javacoding;
+
+public class Student  {
+	//variable types
+	//instance variable
+	int id = 101 ;
+	public static void main (String[]args) {
+		Student student = new Student(); //object
+		System.out.println(student.id);	//instance Acess
+		
+		
+	}	
+}
