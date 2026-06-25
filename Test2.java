be8d9631a4f7bacaa91b71ae9315c73fe702f627
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:07:36 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,15 @@
+package com.javacoding;
+
+public class Test2 {
+	public static void main (String[]args) {
+		Test t1 = new Test();
+		System.out.println(t1.add());
+	}
+
+}
+// test() is a constructor 
+//one class obj can create in another class 
+// we can create within the class or outside the class 
+// some restriction 
+// object class oka instance .class is blue print in class we have variables and methods to access these we can allocate mem, mem allocate chese process a obj creaction
+//once mem allocated methods and variables can access chesukuntam.mem allocated  by creating obj,obj create chesthapudhu internal ga constructors call chestam  
