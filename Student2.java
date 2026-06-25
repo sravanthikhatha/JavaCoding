f59444c3c5706c8b68015673e43e8e434cbc1754
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:06:44 +0530

    Add files via upload

---
+++ Student2.java
@@ -0,0 +1,18 @@
+package com.javacoding;
+
+public class Student2 {
+	static int id = 103 ;
+	String name = "sravs";
+
+	public static void main(String[] args) {
+	System.out.println(id);
+	System.out.println(new Student2().name);
+	int height = 70 ; // method 
+	System.out.println(height);
+	}
+	public static void test1 (){
+	System.out.println(id);
+	
+	}
+}
+//local variable
