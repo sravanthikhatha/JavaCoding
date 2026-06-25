cc7a8e93753b2420b31662ba4990be3c2c82b314
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:03:09 +0530

    Add files via upload

---
+++ ConditionalStatements.java
@@ -0,0 +1,21 @@
+package com.javacoding;
+
+public class ConditionalStatements {
+	public static void main(String[]args) {
+		int i = -10 ;
+		if(i<0) {
+			System.out.println("The given is a negative number");
+		}
+		else {
+			System.out.println("the given is a positive");
+			
+		}
+		
+	}
+
+}
+//if(1>0)
+//sop(positive)  // curly brackets if one statement is there o need of curly brackets .more than one statement curly brackets are mandatory
+//else
+//sop(negative)
+	
