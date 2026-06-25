3b1c698940ad6c5664ef9ec1b066256202f7aee7
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:02:46 +0530

    Add files via upload

---
+++ ConditionalStatement2.java
@@ -0,0 +1,30 @@
+package com.javacoding;
+
+public class ConditionalStatement2 {
+	   public static void main(String[]args) {
+		   int i = -6 ;
+		   switch(i) {
+		   case 1 :
+		System.out.println("this is the first statement");
+			   break ;
+		   case 2:
+			   System.out.println("this is the second  statement");
+			   break ;
+		   case 3 :
+			   System.out.println("this is the third statement");
+			   break ;
+			   default :
+				   System.out.println("this is the default statement");
+				   break;
+		   
+		   }
+		   
+	   }
+	   //int i =1 
+	   // case 1:
+	   //case  2:
+	   // Sop("this is the first statement
+	   // for the n no of statements
+	   // default is option
+
+}
