59d96d4803b9d1265a37f80aab3630053446c518
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:05:02 +0530

    Add files via upload

---
+++ Multiplletimes.java
@@ -0,0 +1,18 @@
+package com.javacoding;
+
+public class Multiplletimes {
+	public static void main(String[]args) {
+		String s = "sravani";
+		 int n= 3;
+				for(int i=0;i<n ;i++) {
+			System.out.println(s);
+			
+	}
+		String me = "sravanthi";
+		int nsravs = 5 ;
+		for(int i =0;i<n ;i++) {
+			System.out.println(me);
+		}
+	}
+
+}
