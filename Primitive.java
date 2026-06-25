05b949ba9148edeecc1f998fd7b36ab33d63999a
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:06:09 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,19 @@
+package com.javacoding;
+
+public class Primitive {
+
+    public static void main(String[] args) {
+        byte b = 50;
+        short s = 250;
+        int i = 214783647;
+        long l = 9876543210L;
+        float f = 3.14f;
+        double d = 88.756421;
+        boolean b1 = true;
+        boolean b2 = false;
+        char c = 'r';
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
