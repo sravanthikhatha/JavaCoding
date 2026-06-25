ed1ed42351a399ade6cfadf1c8925783ab7fa366
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:09:15 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,16 @@
+  package com.javacoding;
+
+public class Variable {
+	public static void main(String[] args) {
+		// DataType VariableName = VariableValue;
+		int pincode = 500058 ;
+		System.out.println(pincode);
+		
+	}
+	
+	}
+
+
+
+
+
