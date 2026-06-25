747f88285e14874a938841ab9435f0470d67ff55
K Sravanthi <24ag5a0507@gmail.com>
2026-06-25 20:00:47 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,17 @@
+package com.javacoding;
+ 
+class Basic {
+
+	public static void main (String[]args) {
+		System.out.println("Hello World");	
+		System.out.println("hello,java");
+
+	}
+
+}
+
+
+
+
+
+
